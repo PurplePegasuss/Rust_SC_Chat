f31266790708ad7fb0c14d765009fdccce5b3015
Kirill Ivanov <bragov4ik@hotmail.com>
2021-05-09 15:05:35 +0300

    Add script for generating identity & certificate. Update .gitignore accordingly

---
+++ keys/gen_x509.sh
@@ -0,0 +1,27 @@
+# CONFIG="
+# # Configuration for openssl req
+# [ req ]
+# default_bits        = 4096
+# distinguished_name  = req_distinguished_name
+# prompt              = no
+# x509_extensions     = v3_req
+
+# [ v3_req ]
+# basicConstraints    = CA:FALSE
+# extendedKeyUsage    = serverAuth, clientAuth
+# subjectAltName      = IP:192.168.0.184
+
+# [ req_distinguished_name ]
+# countryName                     = RU
+# stateOrProvinceName             = Tatarstan
+# localityName                    = Innopolis
+# organizationName                = Innopolis University
+# organizationalUnitName          = BS19
+# commonName                      = *
+
+# emailAddress                   = Email Address
+# "
+
+openssl req -x509 -newkey rsa:4096 -config csr.conf -keyout result/myPrivateKey.pem -out result/certificate.crt -days 365 -nodes
+
+openssl pkcs12 -export -out result/identity.pfx -inkey result/myPrivateKey.pem -in result/certificate.crt -passout pass:localhost
